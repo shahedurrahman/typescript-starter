0c82ee2e2c82902a8ebd008accf7279a746539bf
Shahedur Rahman <shahed.munna@hotmail.com>
2016-02-17 12:33:57 +0600

    Initial check-in.

---
+++ scripts/script.js
@@ -0,0 +1,19 @@
+/**
+ * Person
+ */
+var Person = (function () {
+    function Person() {
+    }
+    Person.prototype.verify = function () {
+        alert(this.firstName + ' ' + this.lastName + '\n' + this.email);
+    };
+    return Person;
+})();
+function foo() {
+    var person = new Person();
+    person.firstName = 'Shahedur';
+    person.lastName = 'Rahman';
+    person.email = 'shahed@outlook.com';
+    person.verify();
+}
+//# sourceMappingURL=script.js.map
---
+++ scripts/script.ts
@@ -0,0 +1,26 @@
+
+/**
+ * Person
+ */
+class Person {
+    firstName: string;
+    lastName: string;
+    email: string;
+
+    constructor() {
+
+    }
+
+    verify() {
+        alert(this.firstName + ' ' + this.lastName + '\n' + this.email);
+    }
+}
+
+function foo() {
+    var person = new Person();
+    person.firstName = 'Shahedur';
+    person.lastName = 'Rahman';
+    person.email = 'shahed@outlook.com';
+
+    person.verify();
+}
